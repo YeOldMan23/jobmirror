114b332e5351ee96a64fc1e44ba6da7ae13cec8c
Jesslyn <79516995+Jesslyn26@users.noreply.github.com>
2025-06-06 13:38:30 +0800

    feat: added loading bronze to pandas, WIP: EDA

--- utils/mongodb_utils.py
+++ utils/mongodb_utils.py
@@ -1,6 +1,7 @@
 import os
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+import pandas as pd
 
 def get_mongo_client() -> MongoClient:
     uri = os.environ.get("MONGO_DB_URL")
@@ -24,3 +25,12 @@ def get_jd_collection(db_name="jobmirror_db", collection_name="bronze_job_descri
 
 def exists_in_collection(collection, doc_id):
     return collection.find_one({"id": doc_id}) is not None
+
+def read_bronze_resumes_as_dataframe(db_name, collection_name, query=None):
+    client = get_mongo_client()
+    collection = client[db_name][collection_name]
+    data = list(collection.find(query or {}))
+    if data and "_id" in data[0]:
+        for d in data:
+            d.pop("_id", None)
+    return pd.DataFrame(data)
