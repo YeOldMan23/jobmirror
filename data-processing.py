6613ecc0461af3f2840e170830db1e274574e79e
Jenpoer <jeistory1@gmail.com>
2025-06-06 23:03:51 +0800

    Change data processing to use pyspark session from mongodb utils

--- data-processing.py
+++ data-processing.py
@@ -4,6 +4,7 @@ import argparse
 
 from pyspark.sql import SparkSession
 
+from utils.mongodb_utils import get_pyspark_session
 from utils.data_processing_bronze_table import process_bronze_table
 
 
@@ -17,13 +18,6 @@ if __name__ == "__main__":
     load_dotenv()
     os.environ["GOOGLE_API_KEY"] = os.getenv("GOOGLE_API_KEY")
 
-    mongodb_uri = os.getenv("MONGO_DB_URL")
-
-    spark = SparkSession.builder \
-        .appName("SaveJSONtoMongoDB") \
-        .config("spark.mongodb.read.connection.uri", mongodb_uri) \
-        .config("spark.mongodb.write.connection.uri", mongodb_uri) \
-        .config("spark.jars.packages", "org.mongodb.spark:mongo-spark-connector_2.12:10.5.0") \
-        .getOrCreate()
+    spark = get_pyspark_session()
     
     process_bronze_table(spark, args.start, args.end, args.batch_size)
