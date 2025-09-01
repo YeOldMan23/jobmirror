0ec87141346b641c88567c892f9f6bdc4d67d162
YeOldMan23 <kieren.chua23@gmail.com>
2025-09-01 16:39:48 +0800

    gunicorn init

---
+++ llm/gunicorn.conf.py
@@ -0,0 +1,23 @@
+# gunicorn.conf.py
+
+# The host and port to bind to
+bind = "0.0.0.0:5000"
+
+# Number of worker processes. A good starting point is (2 x $num_cores) + 1
+workers = 3
+
+# The type of worker class
+worker_class = "sync"
+
+# Timeout for handling a request (in seconds)
+# This is VERY important. If your model takes a long time to generate text,
+# the default 30s timeout will kill the request. Set it to a higher value.
+timeout = 3000  # 50 minutes
+
+# Logging
+loglevel = "info"
+accesslog = "-"  # Log to stdout
+errorlog = "-"   # Log to stderr
+
+# Note : use code 
+# gunicorn --config gunicorn.conf.py app:app
--- llm/llm_flask.py
+++ llm/llm_flask.py
@@ -1,16 +1,28 @@
 # app.py
 from flask import Flask, request, jsonify
 from llama_cpp import Llama
+import argparse
+import os
+
+from download_llama import install_model
+
+# Get env
+MODEL_ID = os.getenv("MODEL_ID", "TheBloke/Mistral-7B-Instruct-v0.2-GGUF")
+MODEL_NAME = os.getenv("MODEL_NAME", "mistral-7b-instruct-v0.2.Q4_K_M.gguf")
+CACHE_DIR = os.path.join(os.getcwd(), "model_cache")
+
+print("---Loading Model---")
+llm_model = install_model(CACHE_DIR,
+                        MODEL_ID,
+                        MODEL_NAME)
+    
 
 # Initialize the Flask app
+print("---Running Application---")
 app = Flask(__name__)
 
-# Load the model globally to avoid reloading on each request
-# Make sure 'model.gguf' is in the same directory as this script
-llm = Llama(model_path="./model.gguf")
-
 @app.route("/generate", methods=["POST"])
-def generate_text():
+def generate_text(llm : Llama):
     data = request.json
     prompt = data.get("prompt", "")
 
@@ -22,6 +34,3 @@ def generate_text():
     generated_text = output["choices"][0]["text"]
 
     return jsonify({"text": generated_text})
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
